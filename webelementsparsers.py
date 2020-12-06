dd521fc01d168b18ffd10b2549f43bcc30f4ab4e
Soroush Hashemi Far <soroush.hashemi76@gmail.com>
2020-12-06 18:11:11 +0330

    bugs fixed

--- webelementsparsers.py
+++ webelementsparsers.py
@@ -32,6 +32,9 @@ class UserAgentParser(Parser):
         ("browserless", r"com\.apple\.WebKit\.WebContent/.+")
     ]
 
+    devices_list = ["WOW64", "WOW", "iPhone", "iPad", "iPod", "Macintosh",
+                    "Linux", "X11", "Win64", "Maemo", "Mobile", "Tablet"]
+
     def __init__(self, user_agent):
         self.user_agent = user_agent
         self.raw_components = []
@@ -71,11 +74,13 @@ class UserAgentParser(Parser):
 
         if self.browser is None and not self.bot_status:
             return None
-        elif self.browser is None and self.bot_status:
-            return self
 
         self._separate_user_agent_components()
         self.components.append(Product(self.raw_components[0]))
+
+        if self.browser is None and self.bot_status:
+            return self
+
         self._extract_details()
 
         return self
@@ -120,6 +125,7 @@ class UserAgentParser(Parser):
     def _extract_details(self):
         if self.browser == "firefox":
             details = self.raw_components[1].split("; ")
+            gecko_release_version = details[-1]
             details.remove(details[-1])
             for term in details:
                 if self._is_device(term):
@@ -141,9 +147,9 @@ class UserAgentParser(Parser):
             else:
                 browser = self.browser.capitalize() + "/"
             self.components.append(Browser(
-                browser, compatibility=compatibility, gecko_release_version=details[-1]))
+                browser, compatibility=compatibility, gecko_release_version=gecko_release_version))
 
-            self.components.append(OS(details))
+            self.components.append(OS(details, self.devices_list))
 
         elif self.browser == "chrome":
             details = self.raw_components[1].split("; ")
@@ -159,7 +165,7 @@ class UserAgentParser(Parser):
                 compatibility = [element for element in self.raw_components[4:]
                                  if "Chrome" not in element and bool(re.match(r".+/.+", element))]
 
-            self.components.append(OS(details))
+            self.components.append(OS(details, self.devices_list))
 
             browser = [element for element in self.raw_components[2:]
                        if "Chrome" in element][0]
@@ -182,7 +188,7 @@ class UserAgentParser(Parser):
                 compatibility = [element for element in self.raw_components[4:]
                                  if "OPR" not in element and bool(re.match(r".+/.+", element))]
 
-            self.components.append(OS(details))
+            self.components.append(OS(details, self.devices_list))
 
             browser = [element for element in self.raw_components[2:]
                        if "OPR" in element][0]
@@ -211,11 +217,11 @@ class UserAgentParser(Parser):
                     compatibility = [element for element in self.raw_components[4:]
                                      if "Safari" not in element and "Version" not in element and "Mobile" not in element and bool(re.match(r".+/.+", element))]
 
+                self.components.append(OS(details, self.devices_list))
+
                 version = [element for element in self.raw_components[4:]
                            if "Version" in element]
                 version = version[0] if len(version) == 1 else None
-                self.components.append(OS(details))
-
                 browser = [element for element in self.raw_components[2:]
                            if "Safari" in element][0]
                 compatibility = [Product(item) for item in compatibility]
@@ -234,7 +240,7 @@ class UserAgentParser(Parser):
                 os_type = [element for element in self.raw_components[1:]
                            if element not in compatibility]
                 os = "macOS"+"; "+os_type[0] if len(os_type) > 0 else "macOS"
-                self.components.append(OS(os))
+                self.components.append(OS(os, self.devices_list))
 
                 compatibility = [Product(item) for item in compatibility]
                 self.components.append(
@@ -248,7 +254,7 @@ class UserAgentParser(Parser):
                     compatibility = [element for element in self.raw_components[1:] if bool(
                         re.match(r".+/.+", element))]
 
-                self.components.append(OS("macOS"))
+                self.components.append(OS("macOS", self.devices_list))
 
                 compatibility = [Product(item) for item in compatibility]
                 self.components.append(
@@ -263,14 +269,17 @@ class UserAgentParser(Parser):
 
             compatibility = details
 
-            self.components.append(OS(self.raw_components[1].split("; ")[2]))
+            self.components.append(
+                OS(self.raw_components[1].split("; ")[2], self.devices_list))
 
             compatibility = [Product(item) for item in compatibility]
             self.components.append(Browser(self.raw_components[1].split(
                 "; ")[1].replace(" ", "/"), compatibility=compatibility))
 
         elif self.browser == "browserless":
-            if bool(re.match(r"Mozilla/.+", self.raw_components[0])):
+            if bool(re.match(r"^Mozilla/[\.\d]+$", self.raw_components[0])):
+                pass
+            elif bool(re.match(r"Mozilla/.+", self.raw_components[0])):
                 details = self.raw_components[1].split("; ")
                 for term in details:
                     if self._is_device(term):
@@ -289,11 +298,11 @@ class UserAgentParser(Parser):
                     compatibility = [element for element in self.raw_components[4:]
                                      if "Mobile" not in element and bool(re.match(r".+/.+", element))]
 
-                self.components.append(OS(details))
+                self.components.append(OS(details, self.devices_list))
 
                 compatibility = [Product(item) for item in compatibility]
-                self.components.append(Browser(
-                    "", compatibility=compatibility))
+                self.components.append(
+                    Browser("", compatibility=compatibility))
                 self.layout_browser_engine = Product(self.raw_components[2])
 
             elif bool(re.match(r"curl/.+", self.raw_components[0])):
@@ -305,7 +314,8 @@ class UserAgentParser(Parser):
                         compatibility = [element for element in self.raw_components[2:] if bool(
                             re.match(r".+/.+", element))]
 
-                    self.components.append(OS(self.raw_components[1]+"; "))
+                    self.components.append(
+                        OS(self.raw_components[1]+"; ", self.devices_list))
 
                     compatibility = [Product(item) for item in compatibility]
                     self.components.append(
@@ -322,7 +332,7 @@ class UserAgentParser(Parser):
                 os_type = [element for element in self.raw_components[1:]
                            if element not in compatibility]
                 os = "macOS"+"; "+os_type[0] if len(os_type) > 0 else "macOS"
-                self.components.append(OS(os))
+                self.components.append(OS(os, self.devices_list))
 
                 compatibility = [Product(item) for item in compatibility]
                 self.components.append(
@@ -337,13 +347,13 @@ class UserAgentParser(Parser):
             self.bot_status = True
             self.components.append(Bot(bot[0][1:-1], URLParser))
 
-        elif bool(re.match(r".+/.+\s?\(\+https?://.+/.*\)", self.user_agent)):
+        elif bool(re.match(r".+/.+\s?\(\+?https?://.+/.*?\)", self.user_agent)):
             self.bot_status = True
             self.components.append(
-                Bot(re.findall(r"\(\+https?://.+/.*\)", self.user_agent)[0][1:-1], URLParser))
+                Bot(re.findall(r"\(\+?https?://.+/.*\)", self.user_agent)[0][1:-1], URLParser))
 
     def _is_device(self, term):
-        if term in ["WOW64", "iPhone", "iPad", "iPod", "Macintosh", "Linux", "X11", "Win64", "Maemo", "Mobile", "Tablet"]:
+        if term in self.devices_list:
             return True
 
         return False
@@ -430,11 +440,11 @@ class URLParser(Parser):
         'ppt', 'odp',
         'doc', 'docx', 'ebook', 'log', 'md', 'msg', 'odt', 'org', 'pages', 'pdf', 'rtf', 'rst', 'tex', 'txt', 'wpd', 'wps',
         '3g2', '3gp', 'aaf', 'asf', 'avchd', 'avi', 'drc', 'flv', 'm2v', 'm4p', 'm4v', 'mkv', 'mng', 'mov', 'mp2', 'mp4', 'mpe', 'mpeg', 'mpg', 'mpv', 'mxf', 'nsv', 'ogg', 'ogv', 'ogm', 'qt', 'rm', 'rmvb', 'roq', 'srt', 'svi', 'vob', 'webm', 'wmv', 'yuv',
-        'html', 'htm', 'css', 'js', 'jsx', 'less', 'scss', 'wasm', 'php'
+        'html', 'htm', 'css', 'js', 'jsx', 'less', 'scss', 'wasm',
     ]
 
     def __init__(self, url):
-        self.url = url
+        self.url = re.sub(r"^\+", "", url)
         self.raw_components = []
         self.components = []
         self.protocol = None
@@ -464,7 +474,7 @@ class URLParser(Parser):
         return flat_dictionary
 
     def parse(self):
-        if not bool(re.match(r"(?i)\b((?:\w+://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))", self.url)):
+        if not bool(re.match(r"\w+://.+/.*?", self.url)):
             return self
 
         self._detect_fragment_ids()
@@ -496,6 +506,8 @@ class URLParser(Parser):
             if len(last_directory) > 1:
                 if last_directory[-1] in self.file_formats:
                     return last_directory[-1]
+                else:
+                    return self.target_accessed
 
     def _detect_fragment_ids(self):
         fragments = re.findall(r"#[A-Za-z_0-9]+$", self.url)
@@ -542,66 +554,3 @@ class URLParser(Parser):
 
             elif isinstance(dictionary[key], str) or isinstance(dictionary[key], int) or isinstance(dictionary[key], float) or isinstance(dictionary[key], bool):
                 new_dictionary[new_key] = dictionary[key]
-
-
-# uas = [
-#     "Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp)",
-#     "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)",
-#     "Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp)",
-#     "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.93 Safari/537.36",
-#     "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
-#     "Mozilla/5.0 (Ubuntu; X11; Linux x86_64; rv:8.0) Gecko/20100101 Firefox/8.0",
-#     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:45.9) Gecko/20100101 Goanna/3.0 Firefox/45.9 PaleMoon/27.0.3",
-#     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36 OPR/42.0.2393.85",
-#     "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)",
-#     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko)",
-#     "Safari/11601.7.7 CFNetwork/760.6.3 Darwin/15.6.0 (x86_64)",
-#     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/600.8.9 (KHTML, like Gecko) Version/8.0.8 Safari/600.8.9',
-#     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko)',
-#     'Mozilla/5.0 (iPhone; CPU iPhone OS 10_2 like Mac OS X) AppleWebKit/602.3.12 (KHTML, like Gecko) Mobile/14C92',
-#     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534+ (KHTML, like Gecko) BingPreview/1.0b',
-#     "curl/7.21.3 (amd64-portbld-freebsd8.2) libcurl/7.21.3 OpenSSL/0.9.8q zlib/1.2.3",
-#     "curl/7.52.1",
-#     'python-requests/2.12.1',
-#     'Python-urllib/2.7',
-#     "Mozilla/5.0 (compatible; MJ12bot/v1.4.7; http://mj12bot.com/)",
-#     "com.apple.WebKit.WebContent/10600.8.9 CFNetwork/720.5.7 Darwin/14.5.0 (x86_64)",
-#     "Sogou web spider/4.0(+http://www.sogou.com/docs/help/webmasters.htm#07)",
-#     "facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)",
-#     "facebookexternalhit/1.1;kakaotalk-scrap/1.0;",
-# ]
-
-# for ua in uas[1:2]:
-#     uap = UserAgentParser(ua)
-#     uap.parse()
-
-#     print(uap.components_as_dictionary())
-#     print(uap.components_as_flat_dictionary())
-
-
-# urls = [
-#     "https://blog.hubspot.com:1234/en/marketing/parts-url",
-#     "ftp://neilpatel.com/blog/complete-guide-structuring-urls/",
-#     "neilpatel.com/blog/complete-guide-structuring-urls/",
-#     "http://localhost:8889/notebooks/Feat%20Eng%20get%20req%20data.ipynb",
-#     'http://www.cwi.nl:80/%7Eguido/Python.html',
-#     "https://www.tutorialrepublic.com/html-tutorial/html-url.php",
-#     "https://webcache.googleusercontent.com/search?q=cache:T5hudFlBksUJ:https://www.tutorialrepublic.com/html-tutorial/html-url.php+&cd=14&hl=en&ct=clnk&gl=ir",
-#     "https://webcache.googleusercontent.com/search?q=cache:T5hudFlBksUJ:https://www.tutorialrepublic.com/html-tutorial/html-url.php+;cd=14;hl=en;ct=clnk;gl=ir#Syntax",
-#     "https://en.wikipedia.org/wiki/URL#Syntax",
-#     "https://themeisle.com/blog/what-is-a-website-url/",
-#     "https://themeisle.com/blog/what-is-a-website-url",
-#     "https://john.doe@www.example.com:123/forum/questions/?tag=networking&order=newest#top",
-#     "https://john.doe@www.example.com:123/forum/questions/search?tag=networking&order=newest#top",
-#     "telnet://192.0.2.16:80/",
-#     "ldap://[2001:db8::7]/c=GB?objectClass?one",
-#     "mailto:John.Doe@example.com",
-#     "tel:+1-816-555-1212",
-#     "news:comp.infosystems.www.servers.unix",
-# ]
-
-# for url in urls:
-#     urlp = URLParser(url)
-#     urlp.parse()
-
-#     print(urlp.components_as_dictionary())
